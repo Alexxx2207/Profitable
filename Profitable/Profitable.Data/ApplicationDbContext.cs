c03e2e3ccc638d33a69f0021bc757035d0de3036
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-07-06 17:38:38 +0300

    Add Posts table

--- Profitable/Profitable.Data/ApplicationDbContext.cs
+++ Profitable/Profitable.Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Profitable.Data
         }
 
         public DbSet<Trader> Traders { get; set; }
+        public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Like> Likes { get; set; }
 
