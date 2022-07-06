c9e7143493bfd40dc95f95a0758c75296db3fd3a
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-07-06 19:12:07 +0300

    Added Repository pattern

---
+++ Profitable/Profitable.Data/Repository/Contract/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profitable.Data.Repository.Contract
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetAsync(string id);
+
+        Task<T> Find(Expression<Func<T, bool>> expression);
+
+        Task<IEnumerable<T>> FindAllWhere(Expression<Func<T, bool>> expression);
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entity);
+
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+
+        void Save();
+    }
+}
