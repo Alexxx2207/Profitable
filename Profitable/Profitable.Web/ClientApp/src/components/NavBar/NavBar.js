d70650ef36dc366bfe27198d652148788ceba736
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-07-15 14:41:20 +0300

    Modify NavBar view

--- Profitable/Profitable.Web/ClientApp/src/components/NavBar/NavBar.js
+++ Profitable/Profitable.Web/ClientApp/src/components/NavBar/NavBar.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
@@ -5,21 +6,21 @@ export const NavBar = () => {
     return (
         <nav className={styles.navbarContainer}>
 
-            <NavLink to="/" className={styles.navbarItems}>
+            <NavLink to="/" className={styles.navLink}>
                 <h1 className={styles.navbarLogo}>PROFITABLE</h1>
             </NavLink>
             <ul className={styles.navbarList}>
                 <li>
-                    <NavLink to="/" className={styles.navbarItems}>HOME</NavLink>
+                    <NavLink to="/" activeClassName={styles.activeLink} className={classNames(styles.navbarListItems, styles.navLink)}>HOME</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/markets" className={styles.navbarItems}>MARKETS</NavLink>
+                    <NavLink to="/markets" activeClassName={styles.activeLink} className={classNames(styles.navbarListItems, styles.navLink)}>MARKETS</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/posts" className={styles.navbarItems}>POSTS</NavLink>
+                    <NavLink to="/posts" activeClassName={styles.activeLink} className={classNames(styles.navbarListItems, styles.navLink)}>POSTS</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about" className={styles.navbarItems}>ABOUT</NavLink>
+                    <NavLink to="/about" activeClassName={styles.activeLink} className={classNames(styles.navbarListItems, styles.navLink)}>ABOUT</NavLink>
                 </li>
             </ul>
         </nav>);
