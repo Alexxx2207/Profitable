12335b0b3ba48f9b4b5acb4a3d0f4e6be70e7311
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-07-19 19:41:09 +0300

    Modify posts list
    - Add FontAwesome
    - Add limited content length for each post

--- Profitable/Profitable.Web/ClientApp/src/components/PostsAndComments/Posts/PostsListItem/PostsListItem.js
+++ Profitable/Profitable.Web/ClientApp/src/components/PostsAndComments/Posts/PostsListItem/PostsListItem.js
@@ -1,13 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 import styles from './PostsListItem.module.css';
 
 export const PostsListItem = (props) => {
     return (<div className={styles.post}>
-        <div className={styles.content}>
+        <div className={styles.titleContent}>
             <h2>{props.title}</h2>
+            <p className={styles.content}>{props.content}</p>
         </div>
         <div className={styles.information}>
             <div className={styles.author}>
                 {props.author}
+            </div>
+            <div className={styles.author}>
+                <FontAwesomeIcon className={styles.iconComments} icon={faComments} />
+                Comments: <strong>{props.comments.length}</strong>
             </div>
             <div className={styles.postedOn}>
                 {props.postedOn}
