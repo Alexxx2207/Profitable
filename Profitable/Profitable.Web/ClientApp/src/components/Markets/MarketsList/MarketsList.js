8491fc2b89dbcdda9f2a8750ea3c0a787f93ae04
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-07-28 10:22:01 +0300

    MarketList style / Navbar style change

--- Profitable/Profitable.Web/ClientApp/src/components/Markets/MarketsList/MarketsList.js
+++ Profitable/Profitable.Web/ClientApp/src/components/Markets/MarketsList/MarketsList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { MarketWidget } from '../MarketWidget/MarketWidget';
-import { getAllInstruments, getInstrument } from'../../../services/markets/marketsService';
+import { getAllInstruments, getInstrument } from '../../../services/markets/marketsService';
+import styles from './MarketsList.module.css';
 
 export const MarketsList = () => {
     const requiredInstrument = {
@@ -26,11 +27,14 @@ export const MarketsList = () => {
     }, []);
 
     return (
-        <div style={{ height: '81vh' }}>
-            <section className="search">
-                <select onChange={(e) => fetchInstrumentData(e.target.value)} value={instrument.tickerSymbol}>
-                    {allInstruments.map(instr => <option key={instr.guid} value={instr.tickerSymbol}>{instr.tickerSymbol}</option>)}
-                </select>
+        <div className={styles.marketsList}>
+            <section className={styles.searchSection}>
+                <div className={styles.searchInstrumentPart}>
+                    <h1 className={styles.chooseInstrumentHeading}>Instrument</h1>
+                    <select className={styles.searchDropdown} onChange={(e) => fetchInstrumentData(e.target.value)} value={instrument.tickerSymbol}>
+                        {allInstruments.map(instr => <option key={instr.guid} value={instr.tickerSymbol}>{instr.tickerSymbol}</option>)}
+                    </select>
+                </div>
             </section>
             <MarketWidget instrument={{ ...instrument }} />
         </div>
