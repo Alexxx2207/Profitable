56e902b1a4f962936b3ac55d395282eb8cb505e0
Alexander Ivanov <alex.ivanov0722@gmail.com>
2022-10-31 15:37:38 +0200

    Update EconomicCalendar.js

--- Profitable/Profitable.Web/ClientApp/src/components/Calendars/EconomicCalendar/EconomicCalendar.js
+++ Profitable/Profitable.Web/ClientApp/src/components/Calendars/EconomicCalendar/EconomicCalendar.js
@@ -1,32 +1,24 @@
 import { useEffect, useState } from "react";
-import { Online, Offline } from "react-detect-offline";
 import { EconomicCalendar } from "react-ts-tradingview-widgets";
 
 import styles from "./EconomicCalendar.module.css";
 
 export const EconomicCalendarPart = () => {
-
-    const [ignored, forceUpdate] = useState('');
+    const [ignored, forceUpdate] = useState("");
 
     useEffect(() => {
-        document.querySelector('#tradingview_widget_wrapper').children[0].style.width = "100%";
-        document.querySelector('#tradingview_widget_wrapper').children[0].style.height = "100%";
-        document.querySelector('#tradingview_widget_wrapper').style.width = "100%";
-        document.querySelector('#tradingview_widget_wrapper').style.height = "85vh";
-        window.addEventListener('resize', forceUpdate);
+        document.querySelector("#tradingview_widget_wrapper").children[0].style.width = "100%";
+        document.querySelector("#tradingview_widget_wrapper").children[0].style.height = "100%";
+        document.querySelector("#tradingview_widget_wrapper").style.width = "100%";
+        document.querySelector("#tradingview_widget_wrapper").style.height = "85vh";
+        window.addEventListener("resize", forceUpdate);
     }, []);
-    
+
     return (
         <div className={styles.calendarsPageContainer}>
-            <Online>
             <div className={styles.economicCalendarContainer}>
                 <EconomicCalendar colorTheme="dark" autosize></EconomicCalendar>
             </div>
-            </Online>
-
-            <Offline>
-            <h1 className={styles.cantShowCalendarMessage}>Economic calendar cannot load. Check Internet Connection.</h1>
-            </Offline>
         </div>
     );
 };
